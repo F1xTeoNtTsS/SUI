f915d69225c6deb73088b2cd1faf354629b0c5d7
F1xTeoNtTsS <F1xTeoNtTsS@gmail.com>
2022-09-15 21:48:43 +0300

    initial commit

---
+++ SUI/SUI/SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIApp.swift
+//  SUI
+//
+//  Created by F1xTeoNtTsS on 15.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
